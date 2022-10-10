45f59119f56d3e429f3d1693ade915d0fbf62698
williamvelichko <williamvelichko2003@gmail.com>
2022-10-10 14:15:33 -0700

    login owrking

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -9,23 +9,58 @@ function LoginForm() {
     email: "",
     password: "",
   });
+
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (userInfo.email === "" || userInfo.password === "") {
+      setError("ALL FIELDS ARE REQUIRED");
+    } else {
+      axios
+        .post("http://localhost:4000/api/auth/login", userInfo)
+        .then((resp) => {
+          localStorage.setItem("token", resp.data.token);
+          push("/projectListings");
+        })
+        .catch((err) => {
+          setError("Wrong Username or Password");
+        });
+    }
+  };
+
   return (
     <div>
       <div className="container">
-        <form>
+        <form onSubmit={submit}>
           <div className="fields">
             <h1>Login</h1>
             <label>Email:</label>
-            <input type="email" name="email" />
+            <input
+              type="email"
+              name="email"
+              value={userInfo.email}
+              onChange={handleChange}
+            />
             <label>Password:</label>
-            <input type="password" name="password" />
+            <input
+              type="password"
+              name="password"
+              value={userInfo.password}
+              onChange={handleChange}
+            />
           </div>
           <div>
             <h3>Dont have an account!</h3>
             <Link to="/signup">Signup</Link>
-            <button id="submit">Login!</button>
+            <button id="submit">Login</button>
           </div>
         </form>
+        <p>{error}</p>
       </div>
     </div>
   );
