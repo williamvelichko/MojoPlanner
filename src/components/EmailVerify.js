566dddec28583844fcf50a01303e0cd897daa940
williamvelichko <williamvelichko2003@gmail.com>
2023-03-28 13:47:07 -0700

    exported and refactored

--- src/components/EmailVerify.js
+++ src/components/EmailVerify.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import { connect } from "react-redux";
